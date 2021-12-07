3a2028df29005bb3cb45cd58cde8a20ed1eda1a7
Lettuce Cobbler Twinkie <64616029+CSeriestechhero@users.noreply.github.com>
2021-12-07 09:13:35 -0800

    Update notepadDev.js

--- services/windo7/notepadDev.js
+++ services/windo7/notepadDev.js
@@ -1,50 +1,14 @@
-const fileDownloadButton = document.getElementById('save');
-function localStorageToFile() {
-    const csv = JSON.stringify(localStorage['autosave']);
-    const csvAsBlob = new Blob([csv], {type: 'text/plain'});
-    const fileNameToSaveAs = 'local-storage.txt';
-    const downloadLink = document.getElementById('save');
-    downloadLink.download = fileNameToSaveAs;
-    if (window.URL !== null) {
-        // Chrome allows the link to be clicked without actually adding it to the DOM
-        downloadLink.href = window.URL.createObjectURL(csvAsBlob);
-        downloadLink.target = `_blank`;
-    } else {
-        downloadLink.href = window.URL.createObjectURL(csvAsBlob);
-        downloadLink.target = `_blank`;
-        downloadLink.style.display = 'none';
-        // add .download so works in Firefox desktop.
-        document.body.appendChild(downloadLink.download);
-    }
-    downloadLink.click();
+function download(filename, text) {
+  var pom = document.createElement('a');
+  pom.setAttribute('href', 'data:text/plain;charset=utf-8,' +
+
+encodeURIComponent(text));
+  pom.setAttribute('download', filename);
+
+  pom.style.display = 'none';
+  document.body.appendChild(pom);
+
+  pom.click();
+
+  document.body.removeChild(pom);
 }
-// file download button event listener
-fileDownloadButton.addEventListener('click', localStorageToFile);
-
-
-(function(console){
-
-    console.save = function(data, filename){
-
-        if(!data) {
-            console.error('Console.save: No data')
-            return;
-        }
-
-        if(!filename) filename = 'console.json'
-
-        if(typeof data === "object"){
-            data = JSON.stringify(data, undefined, 4)
-        }
-
-        var blob = new Blob([data], {type: 'text/json'}),
-            e    = document.createEvent('MouseEvents'),
-            a    = document.createElement('a')
-
-        a.download = filename
-        a.href = window.URL.createObjectURL(blob)
-        a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
-        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
-        a.dispatchEvent(e)
-    }
-})(console)
