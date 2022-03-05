8a66b71c2e92f15c62ecec8ab25e9edc85535c6b
Lettuce Cobbler Twinkie <64616029+CSeriestechhero@users.noreply.github.com>
2022-03-04 22:22:56 -0800

    Update banish.js

--- services/redir/banish.js
+++ services/redir/banish.js
@@ -1,9 +1,12 @@
 $.getJSON("https://api.ipify.org/?format=json", function(e) {
     console.log(e.ip);
-    if (e.ip === '98.167.96.150' || e.ip === '67.220.180.82' || e.ip === '70.173.92.122' || e.ip === '169.241.65.55') {
+    if (e.ip === '98.167.96.150' || e.ip === '70.173.92.122' || e.ip === '169.241.65.55') {
 console.log("bad ip")
 window.location.replace('banned.html')
 } else {
 console.log("piss")
 }
 });
+
+// IP's Vulnerable to banishment
+// Harmony (home):  || e.ip === '67.220.180.82'
